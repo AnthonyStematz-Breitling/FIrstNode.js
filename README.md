3d16e9a61037397a02a0e7fcfb5f71a65809c44f
Anthony Stematz-Breitling <stembreit@gmail.com>
2020-08-10 21:58:27 -0500

    used object deconstruction to plug in inputs and added three todos

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
-# FirstNode.js     [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)                 
-Writing template for Node.js good readme.md application 
+#         undefined         
 
-## Description
+        ## Description
+        
 
-## Table of Contents
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license) 
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-
-
-## Installation
-
-
-## Usage
-
-
-## License 
-
-
-## Contributing
-
-
-## Tests
-
-
-## Questions
+        ## Table of Contents
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [License](#license) 
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [Questions](#questions)
+        
+        
+        ## Installation
+        
+        
+        
+        ## Usage
+        
+        
+        ## License 
+        MIT
+        
+        ## Contributing
+        
+        
+        
+        ## Tests
+        
+        
+        ## Questions
+        * 
+        * 
+        
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
 const util = require("util")
+const { title } = require("process")
 
 const writeFileAsync = util.promisify(fs.writeFile)
-
+//TODO ADD GITHUB LINK THROUGH USERNAME
 inquirer
     .prompt([
     {
@@ -53,21 +54,25 @@ inquirer
         name: "testing"
     },
     ]).then( response =>{
+    
+        let {title, description, installation, usage, license, contributing, testing, github, email} = response
+        //TODO BADGE
         let badge 
         switch(response.license){
-        case "MIT":
-            badge = `[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)`
-        break
-        case "GPlv3":
-            badge = `[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)`
-        break
-        case "AGPL":
-            badge = '[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)'
-        break
-        
-    }
-        writeFileAsync("ReadMe.md",
-        `# ${title}     ${badge}            
+            case "MIT":
+            badge = "[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)"
+            console.log(badge)
+        break;
+        case ["GPlv3"]:
+            badge = "[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)"
+        break;
+        case ["AGPL"]:
+            badge = "[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)"
+        break;
+        }
+        //TODO ADDITIONAL INSTRUCTIONS ON HOW TO CONTACT IN QUESTIONS
+        writeFileAsync("README.md",
+        `# ${title}                 
 
         ## Description
         ${description}
@@ -98,7 +103,7 @@ inquirer
         ## Tests
         ${testing}
         
-        ## Questions
+        ## Questions?
         * ${github}
         * ${email}
         `)
